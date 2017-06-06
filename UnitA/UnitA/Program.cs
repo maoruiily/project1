9d7703d60f4d14ec37205c38f30a89447852b1b7
maorui <maoruiily@163.com>
2017-06-06 11:21:27 +0800

    1

---
+++ UnitA/UnitA/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I'm UnitA，哈哈哈哈");
+            Console.ReadKey();
+        }
+    }
+}
